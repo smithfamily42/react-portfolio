13f6d0a918516099e8323a8506bf47d86a6e286c
Brad Smith <smithfamily42@gmail.com>
2021-08-29 01:31:31 -0700

    reverted back a commit before build and updated resume page

--- src/components/Resume/index.js
+++ src/components/Resume/index.js
@@ -5,19 +5,19 @@ function Resume() {
     <section className="my-5">
       <div className="my-2">
       <p>
-        Download my <a href="https://www.linkedin.com/">resume</a>
+        Download my <a target="_blank" rel="noopener noreferrer" href="https://drive.google.com/file/d/1goZNdQS_e_JEWpZD3FhfPqiBXCrZSGZn/view?usp=sharing">resume</a>
         </p>
-        <h3>Front-end Proficiencies</h3>
+        <h3>Front-end Skills</h3>
         <ul className="skills">
           <li>HTML</li>
           <li>CSS</li>
           <li>JavaScript</li>
           <li>jQuery</li>
-          <li>responsive design</li>
+          <li>Responsive Design</li>
           <li>React</li>
           <li>Bootstrap</li>
         </ul>
-        <h3>Back-end Proficiencies</h3>
+        <h3>Back-end Skills</h3>
         <ul className="skills">
           <li>APIs</li>
           <li>Node</li>
@@ -26,6 +26,7 @@ function Resume() {
           <li>MongoDB, Mongoose</li>
           <li>REST</li>
           <li>GraphQL</li>
+          <li>Progressive Web Apps</li>
         </ul>
       </div>
     </section>
