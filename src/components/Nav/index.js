745ed15d6977c631c1685777456a870dea6f9626
Brad Smith <smithfamily42@gmail.com>
2021-08-30 23:58:57 -0700

    updating nav and README.md

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -16,7 +16,7 @@ function Nav(props) {
 
         <nav className="navbar navbar-expand-md navbar-light">
             <div className="container-fluid">
-                <a className="navbar-brand fs-1 fw-bolder" href="/">Brad Smith's Portfolio</a>
+                <a className="navbar-brand fs-1 fw-bolder" href="https://smithfamily42.github.io/react-portfolio/">Brad Smith's Portfolio</a>
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
                 </button>
