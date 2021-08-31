be50e6dff0c4b54538f3e702c4839521880953cc
Brad Smith <smithfamily42@gmail.com>
2021-08-30 23:42:46 -0700

    updating the about page content

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -6,19 +6,22 @@ function About() {
     <>
       <section className="px-5 my-5 container">
         <div className="my-2 row">
-          <div className="mx-auto my-2 col-lg-4 col-md-4 col-sm-8">
+          <div className="mx-auto my-4 col-lg-4 col-md-4 col-sm-8">
             <img
               src={Profile}
-              alt="Brad Smith Profile Pic" className="img-fluid"
+              alt="Brad Smith Profile Pic" className="img-fluid rounded-circle"
             />
           </div>
-          <p>
-            I am a blossoming full-stack web developer with experience and a passion for learning and growing. I am seeking a full-stack web role that allows me to showcase my diverse, well-rounded skill set.        </p>
-          <p>
-            I provide technical oriented solutions for customers and internal clients while promoting satisfaction and achieving organizational goals. I deliver clean, functional software with memorable results that exceed
-            expectations.</p>
+          <p>I am a blossoming full-stack web developer with experience and a passion for learning and growing.
+            I am seeking a full stack web developer role that allows me to showcase my diverse, well-rounded skill set.
+            I have a bachelor's degree and I have recently earned the full-stack web development certification from University
+            of California - Berkeley’s Coding Bootcamp. I have contributed to and built web presences for government organizations
+            and small business clients/employers.</p>
+          <p>My commitment to growth and development has positioned me to help employers with my new skills and this helps
+            me maintain high levels of motivation and be an impactful contributor. I thrive with front end technologies (HTML, CSS)
+            and leading my team with MySQL and modeling in MVC. Furthermore, I am versed in React, MongoDB, and GraphQL.</p>
         </div>
-      </section>
+    </section>
     </>
   );
 }
